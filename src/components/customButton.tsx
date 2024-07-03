483abbe7b43411f14e415ed20589a11c0dc31b92
Abdul Rehman <abdul.rehman.wb12@gmail.com>
2024-07-03 17:09:27 +0500

    check added

--- src/components/customButton.tsx
+++ src/components/customButton.tsx
@@ -1,7 +1,7 @@
 const CustomButton=()=>{
     return(
         <>
-        <button className="bg-red-500">Click Memsklmdkmksmdasmdlsldmnaslmd asmdsam</button>
+        <button className="bg-red-500">Click Me</button>
         </>
     )
 
